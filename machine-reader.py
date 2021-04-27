e10fefad309f0c43dfb8a41625e86916ca90cd92
Tony <tony@thegulu.com>
2021-04-27 13:36:43 +0800

    Add comment

--- machine-reader.py
+++ machine-reader.py
@@ -3,45 +3,48 @@ import math
 import re
 import nltk
 
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
-# Story class
+
+# article class
 # Contains all information from individual line of test file
 # Contains methods that can be used to aid in score calculation and information retrieval
-class Story:
+class Article:
 
-    # Initialze story object
+    # Initialze article object
     def __init__(self, data):
 
         # Remove symbols
         data = re.sub(r'[\?.\!,-]', '', data)
 
         # Split by tab and save to variables
-        pieces = data.split("\t")
+        seg = data.split("\t")
 
-        self.title = pieces[0]
+        self.title = seg[0]
 
-        self.stuff = pieces[1]
+        self.stuff = seg[1]
 
-        self.text = pieces[2]
+        self.text = seg[2]
         # Text is also turned into a dictionary with word counts
         self.counts = self.counting()
 
         # Questions become a dictionary with the value being a list of answers
-        self.qBank = {}
+        self.qRepo = {}
 
-        self.qBank[pieces[3]] = [pieces[4], pieces[5], pieces[6], pieces[7]]
-        self.qBank[pieces[8]] = [pieces[9], pieces[10], pieces[11], pieces[12]]
-        self.qBank[pieces[13]] = [pieces[14], pieces[15], pieces[16], pieces[17]]
-        self.qBank[pieces[18]] = [pieces[19], pieces[20], pieces[21], pieces[22]]
+        self.qRepo[seg[3]] = [seg[4], seg[5], seg[6], seg[7]]
+        self.qRepo[seg[8]] = [seg[9], seg[10], seg[11], seg[12]]
+        self.qRepo[seg[13]] = [seg[14], seg[15], seg[16], seg[17]]
+        self.qRepo[seg[18]] = [seg[19], seg[20], seg[21], seg[22]]
 
     # Method for obtaining word counts
     def counting(self):
 
-        l = self.text.split()
+        p = self.text.split()
 
         countDict = {}
 
-        for word in l:
+        for word in p:
 
             if word not in countDict.keys():
                 countDict[word] = 1
@@ -50,43 +53,39 @@ class Story:
 
         return countDict
 
-    # Method for caluclating score of word in story
+    # Method for caluclating score of word in article
     def logC(self, word):
         calc = math.log2(1 + (1 / self.counts[word]))
         return calc
 
-    # Output method for testing purposes
     def output(self):
         print("Title: ", self.title)
         print("Extra content: ", self.stuff)
         print("Raw text: ", self.text)
         print("Dict and counts: ", self.counts)
-        print("Question Bank: ", self.qBank)
+        print("Question Repository: ", self.qRepo)
 
 
 # POS Tagging and question type checking
-def checkPOSandQType(currentScore, q, a):
-    aTokens = nltk.word_tokenize(a[0])
-    tag = nltk.pos_tag(aTokens)[0][1]
+def checkPOSandQType(Score, ques, ans):
+    Token = nltk.word_tokenize(ans[0])
+    tag = nltk.pos_tag(Token)[0][1]
 
-    if "Who" in q:
-
+    if "Who" in ques:
         if tag == "NNP" or tag == "NNPS":
-            currentScore += currentScore / 2
+            Score += Score / 2
 
-    if "When" in q:
-
+    if "When" in ques:
         if tag == "CD":
-            currentScore += currentScore
+            Score += Score
 
-    if "What" in q:
-
+    if "What" in ques:
         if tag == "NN" or tag == "NNP" or tag == "NNPS":
-            currentScore += currentScore / 2
+            Score += Score / 2
 
-    return currentScore
+    return Score
 
-
+# map score to 'A' or 'B' or 'C' or 'D'
 def find_mc_answer(scores):
     if max(scores) == scores[0]:
         return 'A'
@@ -98,20 +97,25 @@ def find_mc_answer(scores):
         return 'D'
     return 'A'
 
-
-def get_score(story, question, answer):
-    story_text = story.text.split()
+# get the score of a question answer
+def get_score(article, question, answer):
+    article_text = article.text.split()
 
     question_and_answer = question + answer
+    # find the window size base on the length of the question and answer
     window_size = len(question_and_answer)
+    # convert to set to avoid repeat text
     question_and_answer = set(question_and_answer)
     max_score = 0
-    for i in range(len(story_text) - window_size):
+    # use sliding window method to calculate the score for each answer
+    for i in range(len(article_text) - window_size):
         score = 0
-        sliding_window = story_text[i: i + window_size]
+        # find sliding window base on the index
+        sliding_window = article_text[i: i + window_size]
         for word in question_and_answer:
+            # find if the word inside the question and answer exis at the sliding window
             if word in sliding_window:
-                score += story.logC(word)
+                score += article.logC(word)
         if score > max_score:
             max_score = score
 
@@ -119,17 +123,21 @@ def get_score(story, question, answer):
     return max_score
 
 
-def find_answer(story):
+# find answer of all question inside a article
+def find_answer(article):
     ans = ""
     i = 0
-    for key in story.qBank.keys():
+    # loop though all article
+    for key in article.qRepo.keys():
         question = key.split()
         scores = []
 
-        for answer in story.qBank[key]:
+        # find score for each question
+        for answer in article.qRepo[key]:
             answer = answer.split()
-            scores.append(get_score(story, question, answer))
+            scores.append(get_score(article, question, answer))
 
+        # find answer base on the score
         ans += find_mc_answer(scores)
         if i < 3:
             ans += "\t"
@@ -143,13 +151,13 @@ def do_test(file):
     # read test data
     for line in file.readlines():
         line = line[:-1]
-        data.append(Story(line))
+        data.append(Article(line))
     file.close()
 
     # write answer data
     out = open('ans.txt', 'w')
-    for story in data:
-        out.write(find_answer(story))
+    for article in data:
+        out.write(find_answer(article))
 
 
 do_test('data/MCTest/mc500.test.tsv')
